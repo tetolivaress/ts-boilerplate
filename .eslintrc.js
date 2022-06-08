6c7bd1d982a4a047c8da961a6186d3ee0e3d1744
tetolivaress <tetolivares@gmail.com>
2022-06-08 17:06:01 -0400

    remove trailing comas when running pretttier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: ['plugin:react/recommended', 'standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {}
 }
