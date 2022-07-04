3c7fd797be05069dbdbe503155bc4295faf79952
tetolivares <tetolivares@gmail.com>
2022-07-04 14:38:39 -0400

    Use jest-mock for useLoading hook return value

--- src/__test__/hooks/useItems.test.tsx
+++ src/__test__/hooks/useItems.test.tsx
@@ -1,20 +1,43 @@
-import { renderHook, act, WaitFor } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { Wrapper } from '@utils/tests/LoadingWrapper'
 import { useItems } from '@hooks/useItems'
 import { itemsMock } from '@mocks/items'
 import { waitFor } from '@testing-library/dom'
+import { useLoading } from '../../hooks/useLoading'
+import { mocked } from 'jest-mock'
 
-it('Should have an empty array of items', async () => {
-  const { result } = renderHook(useItems, { wrapper: Wrapper })
-  expect(result.current.items).toEqual([])
-  await act(async () => {
-    await result.current.getItems()
+jest.mock('../../hooks/useLoading')
+const showLoadingMock = jest.fn()
+const hideLoadingMock = jest.fn()
+
+describe('tests of useItems hook', () => {
+  beforeEach(() => {
+    mocked(useLoading).mockReturnValue({
+      isLoading: false,
+      showLoading: showLoadingMock,
+      hideLoading: hideLoadingMock
+    })
   })
-  expect(result.current.items).toEqual(itemsMock)
-})
 
-it('should update array items when after a time', async () => {
-  const { result } = renderHook(useItems, { wrapper: Wrapper })
-  await waitFor(() => expect(result.current.items).toEqual(itemsMock))
-  expect(result.current.items.length).toBeGreaterThan(0)
+  it('Should have an empty array of items', async () => {
+    const { result } = renderHook(useItems, { wrapper: Wrapper })
+    expect(result.current.items).toEqual([])
+    await act(async () => {
+      await result.current.getItems()
+    })
+    expect(result.current.items).toEqual(itemsMock)
+  })
+
+  it('should update array items when after a time', async () => {
+    const { result } = renderHook(useItems, { wrapper: Wrapper })
+    await waitFor(() => expect(result.current.items).toEqual(itemsMock))
+    expect(result.current.items.length).toBeGreaterThan(0)
+  })
+
+  it('should call showLoading and hideLoading method', async () => {
+    const { waitForNextUpdate } = renderHook(useItems, { wrapper: Wrapper })
+    await waitForNextUpdate()
+    expect(showLoadingMock).toHaveBeenCalled()
+    expect(hideLoadingMock).toHaveBeenCalled()
+  })
 })
